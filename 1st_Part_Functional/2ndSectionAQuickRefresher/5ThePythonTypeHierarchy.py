130d5aaace07ab8f50459a9a7efb9edd12b306ef
Patryk Nowak <patryk.r.nowak@gmail.com>
2022-08-18 23:16:13 +0100

    video 5

---
+++ 1st_Part_Functional/2ndSectionAQuickRefresher/5ThePythonTypeHierarchy.py
@@ -0,0 +1,37 @@
+"""
+    The following is a subset of the Python type hierarchy:
+
+                                                Numbers
+                                Integral                    Non-Integral
+                                    Integers                    Floats (c doubles)
+                                    Booleans                    Complex
+                                                                Decimals
+                                                                Fractions
+
+                        
+                                             Collections
+
+        Sequences                             Sets                          Mappings
+
+Mutable         Immutable           Mutable         Immutable               Dictionaries
+- list          - Tuples            - Sets          - Frozen Sets           
+                - Strings
+
+
+                                            Callables (anything what You can invoke)
+
+                            - User-Defined Functions
+                            - Generators
+                            - Classes
+                            - Instance Methods
+                            - Class Instances (__call__())
+                            - Built-in Functions (e.g len(), open())
+                            - Built-in Methods (e.g. my_list.append(x))
+
+
+                                            Singletons
+
+                            - None
+                            - NotImplemented
+                            - Ellipsis(...)
+"""
