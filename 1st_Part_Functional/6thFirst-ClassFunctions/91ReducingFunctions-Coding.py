a42411c61f5dac1059e6796d6e91ee03493ce546
Patryk Nowak <patryk.r.nowak@gmail.com>
2022-11-07 22:25:01 +0000

    reducing functions - code

---
+++ 1st_Part_Functional/6thFirst-ClassFunctions/91ReducingFunctions-Coding.py
@@ -0,0 +1,48 @@
+# Reducing Functions in Python
+
+
+from functools import reduce
+l = [5, 8, 6, 10, 9]
+
+
+def _max(a, b): return a if a > b else b
+
+
+def max_sequence(sequence):
+    result = sequence[0]
+    for x in sequence[1:]:
+        result = _max(result, x)
+    return result
+
+
+print(max_sequence(l))
+
+print("*" * 80)
+
+
+def _reduce(fn, sequence):
+    result = sequence[0]
+    for x in sequence[1:]:
+        result = fn(result, x)
+    return result
+
+
+print(_reduce(_max, l))
+
+
+def _min(a, b): return a if a < b else b
+
+
+print(_reduce(_min, l))
+print("*" * 80)
+
+# ----------------------------------------------------------------
+
+print(reduce(lambda a, b: a if a > b else b, l))
+print(reduce(lambda a, b: a if a < b else b, l))
+print(reduce(lambda a, b: a + b, l))
+print("*" * 80)
+# ----------------------------------------------------------------
+
+l = []
+print(reduce(lambda x, y: x*y, l, 0))
