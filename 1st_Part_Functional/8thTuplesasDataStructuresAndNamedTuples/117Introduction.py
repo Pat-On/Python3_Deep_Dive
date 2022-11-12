8441dd29e058cec971de1d54b00c66aa2976edff
Patryk Nowak <patryk.r.nowak@gmail.com>
2022-11-12 17:43:29 +0000

    Tuples - intro

---
+++ 1st_Part_Functional/8thTuplesasDataStructuresAndNamedTuples/117Introduction.py
@@ -0,0 +1,21 @@
+"""
+        Tuples
+
+Tuples are...
+
+
+    read-only lists...  at least that is how many introductions to Python will present tuples!!
+
+    This is not wrong, but there is a lot more going on with tuples...
+
+    If You only think of tuples as read-only lists, you are going to miss out on some interesting ideas
+
+    We really need to think of tuples also as data records <--- position of values has meaning
+
+    This is why we are going to start looking at tuples before we even cover sequences types
+
+    we are going to focus here on tuples as a data records of structures
+
+    We will also look at named tuples
+
+"""
